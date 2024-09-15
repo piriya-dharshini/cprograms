d6bd5082eedbb91b43f8fa13d2ef27a99a24680c
piriyadharshini <piriyadharshini.a@gmail.com>
2024-09-15 20:19:49 +0530

    second-commit

---
+++ dttypes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    /* int,double,char,float
+    usually double is used for floating numbers
+    char-%d(the number is displayed)*/
+
+    int age=18;
+    double rise=5e6;
+    char alph='z';
+    float val=5.509;
+
+    printf("%d\n",age);
+    printf("%lf\n",rise);
+    printf("%d\n",alph);
+    printf("%c\n",alph);
+    printf("%0.1f\n",val);
+
+}
---
+++ userip.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int age;
+
+    // in settings set the preferences as integrated code terminal
+    // Prompt the user to enter their age
+    printf("Enter the value: ");
+    
+    // Ensures the output appears immediately
+
+    // Reads the input value for 'age'
+    scanf("%d", &age);
+
+    // Print the entered age
+    printf("My age is: %d\n", age);
+
+    return 0;
+}
+
